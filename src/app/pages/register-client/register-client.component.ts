851adedba0081b0d402457c4f95d7a749b567d5f
ilhemboud05 <ilhemmiliboud@gmail.com>
2026-05-02 18:07:47 +0100

    numero par pays

--- src/app/pages/register-client/register-client.component.ts
+++ src/app/pages/register-client/register-client.component.ts
@@ -12,7 +12,40 @@ import { HttpClient } from '@angular/common/http';
   styleUrl: './register-client.component.css'
 })
 export class RegisterClientComponent {
+   countries = [
+  { name: 'Algérie',      flag: '🇩🇿', dialCode: '+213', digits: 9,  placeholder: '5XX XXX XXX' },
+  { name: 'France',       flag: '🇫🇷', dialCode: '+33',  digits: 9,  placeholder: '6XX XXX XXX' },
+  { name: 'Maroc',        flag: '🇲🇦', dialCode: '+212', digits: 9,  placeholder: '6XX XXX XXX' },
+  { name: 'Tunisie',      flag: '🇹🇳', dialCode: '+216', digits: 8,  placeholder: 'XX XXX XXX'  },
+  { name: 'Égypte',       flag: '🇪🇬', dialCode: '+20',  digits: 10, placeholder: '1XX XXX XXXX'},
+  { name: 'Espagne',      flag: '🇪🇸', dialCode: '+34',  digits: 9,  placeholder: '6XX XXX XXX' },
+  { name: 'Italie',       flag: '🇮🇹', dialCode: '+39',  digits: 10, placeholder: '3XX XXX XXXX'},
+  { name: 'Allemagne',    flag: '🇩🇪', dialCode: '+49',  digits: 10, placeholder: '1XX XXXXXXX' },
+  { name: 'Royaume-Uni',  flag: '🇬🇧', dialCode: '+44',  digits: 10, placeholder: '7XXX XXXXXX' },
+  { name: 'États-Unis',   flag: '🇺🇸', dialCode: '+1',   digits: 10, placeholder: 'XXX XXX XXXX'},
+  { name: 'Canada',       flag: '🇨🇦', dialCode: '+1',   digits: 10, placeholder: 'XXX XXX XXXX'},
+  { name: 'Arabie Saoudite', flag: '🇸🇦', dialCode: '+966', digits: 9, placeholder: '5XX XXX XXX'},
+  { name: 'Émirats',      flag: '🇦🇪', dialCode: '+971', digits: 9,  placeholder: '5XX XXX XXX' },
+  { name: 'Turquie',      flag: '🇹🇷', dialCode: '+90',  digits: 10, placeholder: '5XX XXX XXXX'},
+];
 
+selectedCountry = this.countries[0];
+phoneNumber = '';
+
+onCountryChange() {
+  this.phoneNumber = '';
+  this.client.numTel = 0;
+}
+
+onPhoneChange() {
+  const digits = this.phoneNumber.replace(/\D/g, '');
+  this.client.numTel = parseInt(this.selectedCountry.dialCode.replace('+', '') + digits) || 0;
+}
+
+isPhoneValid(): boolean {
+  const digits = this.phoneNumber.replace(/\D/g, '');
+  return digits.length === this.selectedCountry.digits;
+}
   client = {
     nomUtil: '',
     prenomUtil: '',
@@ -47,7 +80,10 @@ export class RegisterClientComponent {
   const pwd = this.client.motDePasse;
 
   const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]).{8,12}$/;
-
+    if (this.phoneNumber && !this.isPhoneValid()) {
+  this.errorMessage = `Le numéro doit contenir exactement ${this.selectedCountry.digits} chiffres pour ${this.selectedCountry.name} (${this.selectedCountry.dialCode}).`;
+  return;
+}
   if (!passwordRegex.test(pwd)) {
     this.errorMessage = 'Le mot de passe doit contenir 8 à 12 caractères, au moins une majuscule, un chiffre et un caractère spécial.';
     return;
